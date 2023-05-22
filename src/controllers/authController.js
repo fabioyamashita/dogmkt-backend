f4f4ef8672fbd00a72e9df5dc22156b2ca46736f
Fabio Yamashita <98363297+fabioyamashita@users.noreply.github.com>
2023-05-22 08:53:21 -0300

    #52 refactor: Add authController refactor functions and using services and repositories (#57)

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,20 +1,11 @@
-const crypto = require("crypto");
 const { promisify } = require("util");
 const jwt = require("jsonwebtoken");
-const User = require("../models/userModel");
+const userService = require("../services/userService");
 const AppError = require("../utils/appError");
-
-const signToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
-};
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
-
-  // Remove password from output
-  user.password = undefined;
+  removePasswordFromOutput(user);
 
   res.status(statusCode).json({
     data: {
@@ -24,11 +15,19 @@ const createSendToken = (user, statusCode, res) => {
   });
 };
 
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+const removePasswordFromOutput = (user) => user.password = undefined;
+
 exports.signup = async (req, res, next) => {
-  await User.create({
+  const user = await userService.create({
     ...req.body,
 
-    // ...req.body is a short way to do this
+    // ...req.body is a short way to do this (Spread operator)
     // name: req.body.name,
     // email: req.body.email,
     // password: req.body.password,
@@ -36,54 +35,69 @@ exports.signup = async (req, res, next) => {
     // isSeller: req.body.isSeller
   });
 
-  res.status(201).json({
-    status: "success",
-  });
+  if (!user) {
+    throw new AppError(422, "The request was well-formed but unable to be followed due to semantic errors.", "Try again later!");
+  };
+
+  res.status(204).send();
 };
 
 exports.login = async (req, res, next) => {
+  // 1) Check if email and password exist
+  if (!isValidLoginRequest(req.body)) {
+    throw new AppError(400, "The request contains malformed data in parameters.", "Please provide email and password!");
+  };
+
   const { email, password } = req.body;
 
-  // 1) Check if email and password exist
-  if (!email || !password) {
-    throw new AppError(400, "The request contains malformed data in parameters.", "Please provide email and password!");
-  }
-
   // 2) Check if the user exists && password is correct
-  const user = await User.findOne({ email }).select("+password");
+  const user = await userService.findByEmailWithPassword(email);
 
-  if (!user || !(await user.correctPassword(password, user.password))) {
+  if (!(await isUserValidated(user, password))) {
     throw new AppError(401, "Incorrect email or password!", "");
-  }
+  };
 
   // 3) If everything ok, send token to client
   createSendToken(user, 200, res);
 };
 
-exports.protect = async (req, res, next) => {
-  // 1) Getting token and check if it's there
-  let token;
+const isValidLoginRequest = (requestBody) => {
+  const { email, password } = requestBody;
+  return email && password;
+};
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    token = req.headers.authorization.split(" ")[1];
-  }
+const isUserValidated = async (user, password) => { 
+  return user && await user.correctPassword(password, user.password);
+};
+
+exports.protect = async (req, res, next) => {
+  // 1) Getting token from request headers and check if it's there
+  const token = getTokenFromHeaders(req.headers);
 
   if (!token) {
     throw new AppError(401, "Unauthorized.", "Authentication credentials are missing or invalid.");
-  }
+  };
 
   // 2) Verification token
-  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  const decodedToken = await decodeToken(token);
 
   // 3) Check if user exists
-  const currentUser = await User.findById(decoded.id);
+  const currentUser = await userService.findById(decodedToken.id);
   if (!currentUser) {
     throw new AppError(401, "Unauthorized.", "Authentication credentials are missing or invalid.");
-  }
+  };
 
   // GRANT ACCESS TO PROTECTED ROUTE
   next();
+};
+
+const getTokenFromHeaders = (headers) => {
+  if (headers.authorization && headers.authorization.startsWith('Bearer')) {
+    return headers.authorization.split(" ")[1];
+  }
+  return null;
+};
+
+const decodeToken = async (token) => {
+  return await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 };
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,16 @@
 const userRepository = require("../repositories/userRepository")
 
 exports.findById = async (id) => {
-    let user = await userRepository.findById(id);
-    return user;
-  };
+  let user = await userRepository.findById(id);
+  return user;
+};
+
+exports.findByEmailWithPassword = async (email) => {
+  let user = await userRepository.findByEmailWithPassword(email);
+  return user;
+};
+
+exports.create = async (userDTO) => {
+  let user = await userRepository.create(userDTO);
+  return user;
+};
