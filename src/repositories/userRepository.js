26fee651d53a3dd596ec20023d0d09885ffacea6
Fabio Yamashita <yamashitafabio@gmail.com>
2023-05-21 22:36:54 -0300

    #52 refactor: Add authController refactor functions and using services and repositories

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -3,3 +3,11 @@ const User = require("../models/userModel");
 exports.findById = async (id) => {
   return await User.findById(id);
 };
+
+exports.findByEmailWithPassword = async (email) => {
+  return await User.findOne({ email }).select("+password");
+};
+
+exports.create = async (userDTO) => {
+  return await User.create(userDTO);
+};
