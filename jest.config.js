699272f2a068bba5ec08f22416136c9277220143
Fabio Yamashita <yamashitafabio@gmail.com>
2023-06-02 22:04:36 -0300

    config: Add config files for tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  testEnvironment: 'node',
+  // testEnvironment: 'node', // This can conflict with jest-mongodb
   testMatch: ['**/*.test.js'],
   moduleFileExtensions: ['js', 'jsx'],
   setupFilesAfterEnv: [],
+  preset: '@shelf/jest-mongodb',
+  watchPathIgnorePatterns: ['globalConfig']
 };
