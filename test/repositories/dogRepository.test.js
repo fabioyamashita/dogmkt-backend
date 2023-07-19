1f78f48d7715e3060dbd8ef47af2d6eef3539909
Fabio Yamashita <yamashitafabio@gmail.com>
2023-07-18 22:34:38 -0300

    test: Add dogRepository tests

--- test/repositories/dogRepository.test.js
+++ test/repositories/dogRepository.test.js
@@ -1,9 +1,7 @@
 const Dog = require("../../src/models/dogModel");
-const mongoose = require('mongoose');
 const dogRepository = require("../../src/repositories/dogRepository");
 const sinon = require("sinon");
-
-const { mockDog } = require('../mocks/dog.mock');
+const { mockDog, mockArrayDogs } = require('../mocks/dog.mock');
 
 describe("dogRepository.create Tests", () => {
   afterEach(() => jest.clearAllMocks());
@@ -32,5 +30,107 @@ describe("dogRepository.create Tests", () => {
     // Assert
     expect(Dog.create).toHaveBeenCalledWith(mockDog);
     expect(dog).toEqual(null);
+  });
+});
+
+describe("dogRepository.countDocuments Tests", () => {
+  afterEach(() => jest.clearAllMocks());
+  Dog.countDocuments = jest.fn();
+
+  it('should successfully count the number of documents in the DB', async () => {
+    // Arrange
+    Dog.countDocuments.mockResolvedValueOnce(10);
+
+    // Act
+    const result = await dogRepository.countDocuments({});
+
+    // Assert
+    expect(result).toEqual(10);
+  });
+
+  it('should return 0 when trying to count the number of documents in the DB and an error occurs', async () => {
+    // Arrange
+    Dog.countDocuments.mockRejectedValueOnce(null);
+
+    // Act
+    const result = await dogRepository.countDocuments({});
+
+    // Assert
+    expect(result).toEqual(0);
+  });
+
+  it('should return 0 when trying to count the number of documents in the DB and the result is null', async () => {
+    // Arrange
+    Dog.countDocuments.mockResolvedValueOnce(null);
+
+    // Act
+    const result = await dogRepository.countDocuments({});
+
+    // Assert
+    expect(result).toEqual(0);
+  });
+});
+
+describe("dogRepository.getAll Tests", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+    sinon.restore();
+  });
+
+  it('should successfully get all documents in the DB', async () => {
+    // Arrange
+    const query = {};
+    const page = 1;
+    const limit = 10;
+
+    Dog.find = sinon.stub().callsFake(() => ({
+      skip: sinon.stub().callsFake(() => ({
+        limit: sinon.stub().resolves(mockArrayDogs)
+      })),
+    }));
+
+    // Act
+    const result = await dogRepository.getAll(query, page, limit);
+
+    // Assert
+    expect(result).toEqual(mockArrayDogs);
+  });
+
+  it('should return an empty array when trying to get all documents in the DB and the result is null', async () => {
+    // Arrange
+    const query = {};
+    const page = 1;
+    const limit = 10;
+
+    Dog.find = sinon.stub().callsFake(() => ({
+      skip: sinon.stub().callsFake(() => ({
+        limit: sinon.stub().resolves(null)
+      })),
+    }));
+
+    // Act
+    const result = await dogRepository.getAll(query, page, limit);
+
+    // Assert
+    expect(result).toEqual([]);
+  });
+
+  it('should return an empty array when trying to get all documents in the DB and an error occurs', async () => {
+    // Arrange
+    const query = {};
+    const page = 1;
+    const limit = 10;
+
+    Dog.find = sinon.stub().callsFake(() => ({
+      skip: sinon.stub().callsFake(() => ({
+        limit: sinon.stub().rejects(null)
+      })),
+    }));
+
+    // Act
+    const result = await dogRepository.getAll(query, page, limit);
+
+    // Assert
+    expect(result).toEqual([]);
   });
 });
